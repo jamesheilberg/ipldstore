202e45c76b1e703dbd1c7d0b4e4151d8d5c519af
eschechter <schechter.evan@gmail.com>
2022-07-06 08:31:00 -0700

    update test to have correct return logic

--- test/test_metadata_tree.py
+++ test/test_metadata_tree.py
@@ -2,8 +2,8 @@ from ipldstore import get_ipfs_mapper
 import ipfshttpclient
 
 def tree_satisfies_limits(cid_or_dict, limit):
-    ipfs = ipfshttpclient.connect()
     if isinstance(cid_or_dict, str):
+        ipfs = ipfshttpclient.connect()
         d = ipfs.dag.get(cid_or_dict).as_json()
     else:
         d = cid_or_dict
@@ -13,27 +13,30 @@ def tree_satisfies_limits(cid_or_dict, limit):
         if isinstance(d[k], dict):
             if "/" in d[k] and not tree_satisfies_limits(d[k]["/"], limit):
                 return False
-            else:
-                return tree_satisfies_limits(d[k], limit)
+            elif not tree_satisfies_limits(d[k], limit):
+                return False
     return True
-    
-ORIGINAL_DICT = {'0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': {'10': 10,
-              '11': 11,
-              '12': 12,
-              '13': 13,
-              '14': 14,
-              '15': 15,
-              '16': 16,
-              '17': 17,
-              "18": 18,
-              "19": 19},
-        '6': 6,
-        '7': 7}
+
+
+ORIGINAL_DICT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': {
+        '10': 10,
+        '11': 11,
+        '12': 12,
+        '13': 13,
+        '14': 14,
+        '15': 15,
+        '16': 16,
+        '17': 17,
+        "18": 18,
+        "19": 19},
+    '6': 6,
+    '7': 7}
 
 LIMIT = 3
 
